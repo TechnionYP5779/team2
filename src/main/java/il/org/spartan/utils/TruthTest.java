40a7cc642f0c989c1fb3e5b4088e1c9e58839810
JS_MM <jsmm0202@gmail.com>
2018-10-31 15:15:31 +0200

    issue #19

--- src/main/java/il/org/spartan/utils/TruthTest.java
+++ src/main/java/il/org/spartan/utils/TruthTest.java
@@ -9,7 +9,16 @@ import org.junit.*;
 
 public class TruthTest {
   @Test public void truthOfTest() {
+    BooleanSupplier bt = ()->true;
+    Truth t = Truth.truthOf(bt); 
+    assert t == Truth.T;
     
+    BooleanSupplier bf = ()->false;
+    t = Truth.truthOf(bf); 
+    assert t == Truth.F;
+    
+    t = Truth.truthOf(null); 
+    assert t == Truth.N;
   }
   
   @Test public void notTest() {
@@ -32,4 +41,35 @@ public class TruthTest {
     assert t.or(Truth.T) == Truth.T;
     assert t.or(Truth.F) == Truth.F;
   }
+  
+  @Test public void andTest() {
+    BooleanSupplier bt = ()->true;
+    Truth t = Truth.truthOf(bt); 
+    assert t.and(Truth.F) == Truth.F;
+    assert t.and(Truth.T) == Truth.T;
+  
+    BooleanSupplier bf = ()->false;
+    t = Truth.truthOf(bf);
+    assert t.and(Truth.T) == Truth.F;
+    assert t.and(Truth.F) == Truth.F;
+  }
+  
+  @Test public void letterOfTest() {
+   // BooleanSupplier bt = ()->true;
+   // assert Truth.letterOf(bt) == Truth.truthOf(bt)+"";   
+  }
+  
+  @Test public void toStringTest() {
+    BooleanSupplier bt = ()->true;
+    Truth t = Truth.truthOf(bt); 
+    assert t.toString() == "true";
+    
+    BooleanSupplier bf = ()->false;
+    t = Truth.truthOf(bf); 
+    assert t.toString() == "false";
+    
+    t = Truth.truthOf(null); 
+    assert t.toString() == "Null pointer exception";
+   }
+  
 }
