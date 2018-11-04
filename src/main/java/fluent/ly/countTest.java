0573e4c73cf0416cf00d3330207c5aaf2dd5bd19
Dor Kabareti <DorKabareti@Dors-MacBook-Pro.local>
2018-11-04 16:37:09 +0200

    closes #24

---
+++ src/main/java/fluent/ly/countTest.java
@@ -0,0 +1,34 @@
+package fluent.ly;
+
+import static org.junit.Assert.*;
+import java.util.*;
+
+import org.junit.*;
+
+public class countTest {
+  private List<Integer> l;
+  
+  @Before
+  public void setUp() {
+    l = new ArrayList<>();
+  }
+  
+  
+  @Test public void testCreateEmpty() {
+    assert(count.of(l)==0);
+   
+  }
+  
+  @Test public void testAdd() {
+    l.add(3);
+    l.add(2);
+    assert(count.of(l)==2);
+  }
+  
+  
+  @Test public void testAddRemove() {
+    l.add(5);
+    l.remove(0);
+    assert(count.of(l)==0);
+  }
+}
