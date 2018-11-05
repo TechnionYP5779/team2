01ac71b10419bebd2a1bb5b6971f0a2d80a81dc5
Nerya Cohen <nerya50@gmail.com>
2018-11-05 16:04:40 +0200

    countTest.java is now 100% covered

--- src/main/java/fluent/ly/countTest.java
+++ src/main/java/fluent/ly/countTest.java
@@ -4,30 +4,27 @@ import java.util.*;
 
 import org.junit.*;
 
-@SuppressWarnings("boxing")public class countTest {
+@SuppressWarnings("boxing") public class countTest {
   private List<Integer> l;
-  
-  @Before
-  public void setUp() {
+
+  @Before public void setUp() {
     l = new ArrayList<>();
   }
-  
-  
+
   @Test public void testCreateEmpty() {
-    assert(count.of(l)==0);
+    Assert.assertEquals(0, count.of(l));
   }
-  
- @Test public void testAdd() {
+
+  @Test public void testAdd() {
     l.add(3);
     l.add(2);
     l.add(111);
-    assert(count.of(l)==3);
+    Assert.assertEquals(3, count.of(l));
   }
-  
-  
+
   @Test public void testAddRemove() {
     l.add(5);
     l.remove(0);
-    assert(count.of(l)==0);
+    Assert.assertEquals(0, count.of(l));
   }
 }
