aa523e9d1d34be3cbc186366affc0614e84c3f44
Nerya Cohen <nerya50@gmail.com>
2018-11-08 00:45:17 +0200

    spartanize the code - Fixes #41

--- src/main/java/fluent/ly/idiomaticTest.java
+++ src/main/java/fluent/ly/idiomaticTest.java
@@ -7,63 +7,59 @@ import fluent.ly.idiomatic.*;
 import java.io.IOException;
 
 @SuppressWarnings("static-method") public class idiomaticTest {
-  @SuppressWarnings("null") @Test public void testIncaseTrueReturnSameObj() {
-    Integer integer = new Integer(5);
+  @Test @SuppressWarnings("null") public void testIncaseTrueReturnSameObj() {
+    Integer integer = Integer.valueOf(5);
     assert idiomatic.incase(true, integer).equals(integer);
   }
 
   @Test public void testIncaseFalseReturnsNull() {
-    assert idiomatic.incase(false, new Integer(5)) == null;
+    assert idiomatic.incase(false, Integer.valueOf(5)) == null;
   }
 
-  @SuppressWarnings("null") @Test public void testKatchingWithNoException() {
-    final Producer<Integer> p = () -> new Integer(5);
-    assert idiomatic.katching(p).equals(new Integer(5));
+  @Test @SuppressWarnings("null") public void testKatchingWithNoException() {
+    assert idiomatic.katching((Producer<Integer>) () -> Integer.valueOf(5)).equals(Integer.valueOf(5));
   }
 
-  @SuppressWarnings("null") @Ignore @Test public void testKatchingWithException() {
-    final Producer<Integer> p = () -> {
+  @Ignore @Test @SuppressWarnings("null") public void testKatchingWithException() {
+    assert idiomatic.katching((Producer<Integer>) () -> {
       throw new IOException();
-    };
-    assert idiomatic.katching(p) == (null);
+    }) == null;
   }
 
   @Test public void testQuoteNotNullString() {
-    assert idiomatic.quote("hello").equals("'hello'");
+    assert "'hello'".equals(idiomatic.quote("hello"));
   }
 
   @Test public void testQuoteNullString() {
-    assert idiomatic.quote(null).equals("<null reference>");
+    assert "<null reference>".equals(idiomatic.quote(null));
   }
 
   @Test public void testDoubleQuote() {
-    assert idiomatic.quote(idiomatic.quote("a")).equals("''a''");
+    assert "''a''".equals(idiomatic.quote(idiomatic.quote("a")));
   }
 
-  @SuppressWarnings("null") @Test public void testWhenTrueReturnsSame() {
-    assert idiomatic.when(true).eval(() -> new Integer(5)).equals(new Integer(5));
+  @Test @SuppressWarnings("null") public void testWhenTrueReturnsSame() {
+    assert idiomatic.when(true).eval(() -> Integer.valueOf(5)).equals(Integer.valueOf(5));
   }
 
   @Test public void testWhenFalseReturnsNull() {
-    assert idiomatic.when(1 == 3).eval(() -> new Integer(5)) == null;
+    assert idiomatic.when(1 == 3).eval(() -> Integer.valueOf(5)) == null;
   }
-  
-  @SuppressWarnings("null") @Test public void testUnlessFalseReturnsSame() {
-    assert idiomatic.unless(1 == 3).eval(() -> new Integer(5)).equals(new Integer(5));
+
+  @Test @SuppressWarnings("null") public void testUnlessFalseReturnsSame() {
+    assert idiomatic.unless(1 == 3).eval(() -> Integer.valueOf(5)).equals(Integer.valueOf(5));
   }
 
   @Test public void testUnlessTrueReturnsNull() {
-    assert idiomatic.unless(4 == 4).eval(() -> new Integer(5)) == null;
+    assert idiomatic.unless(true).eval(() -> Integer.valueOf(5)) == null;
   }
-  
+
   @Test public void testUnlessFalseWithoutEval() {
-    Integer i = new Integer(5);
+    Integer i = Integer.valueOf(5);
     assert idiomatic.unless(false, i) == i;
   }
-  
+
   @Test public void testUnlessTrueWithoutEval() {
-    Integer i = new Integer(5);
-    assert idiomatic.unless(true, i) == null;
+    assert idiomatic.unless(true, Integer.valueOf(5)) == null;
   }
-  
 }
