081462ff158da3728c4b957e7382cf1aba001ad1
DorKab <dor.kabareti@campus.technion.ac.il>
2018-11-10 16:23:18 +0200

    range.from(2) - issue #44

--- src/main/java/il/org/spartan/utils/Range.java
+++ src/main/java/il/org/spartan/utils/Range.java
@@ -1,5 +1,23 @@
+package il.org.spartan.utils;
+
+
 public class Range{
-  /**/
+  
+  private int from;
+  private int to;
+
+  public Range() {
+    this.from = Integer.MIN_VALUE;
+    this.to = Integer.MAX_VALUE;
+  }
+  
+  public void from(int ¢) {
+    this.from = ¢;
+  }
+
+  public boolean includes(int ¢) {
+    return ¢>=this.from && ¢<= this.to;
+  }
 }
 
 
--- src/test/java/il/org/spartan/utils/RangeTest.java
+++ src/test/java/il/org/spartan/utils/RangeTest.java
@@ -1,4 +1,25 @@
+package il.org.spartan.utils;
+
+
+//import fluent.ly.azzert;
+import org.junit.*;
+
+
 public class RangeTest{
-  /**/
+
+  @Test 
+  public void testInfiniteRange() {
+    Range r = new Range();
+    r.from(2);
+    
+    assert(r.includes(2));
+    assert(r.includes(10));
+    assert(r.includes(Integer.MAX_VALUE));
+    
+    assert(!r.includes(1));
+    assert(!r.includes(-7));
+    
+    
+  }
 }
 
