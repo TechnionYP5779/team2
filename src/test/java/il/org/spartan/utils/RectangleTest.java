c76ad178e3952d4014b397e7c58db978dbf9c561
Tal <talporat@campus.technion.ac.il>
2018-11-14 11:35:41 +0200

    fixes rectangle ctor tests #46

--- src/test/java/il/org/spartan/utils/RectangleTest.java
+++ src/test/java/il/org/spartan/utils/RectangleTest.java
@@ -5,102 +5,77 @@ import org.junit.*;
 import fluent.ly.*;
 import il.org.spartan.etc.*;
 
-@SuppressWarnings("static-method")
+@SuppressWarnings({"static-method","static-access"})
 public class RectangleTest {
-  @Test  public void testCtor() throws Exception {
+  @Test  public void testCtor1() throws IllegalArgumentException {
     azzert.assertNotEquals(new Rectangle(1, 2, 3, 4), null);
+  }
+  @Test  public void testCtor2() throws IllegalArgumentException {
     azzert.assertNotEquals(new Rectangle(4, 3, 2, 1), null);
   }
   @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms1() {
-      forget.it(new Rectangle(1, 2, 1, 2));
+    forget.it(new Rectangle(1, 2, 1, 2));
   }
   @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms2() {
     forget.it(new Rectangle(5, 2, 5, 2));
   }
-
-//  @Test  public void testInvalidCtorParms() {
-//    try {
-//      new Rectangle(1, 2, 1, 2);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//    try {
-//      new Rectangle(5, 2, 5, 2);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//    try {
-//      new Rectangle(1, 2, 1, 4);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//    try {
-//      new Rectangle(5, 2, 5, 4);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//    try {
-//      new Rectangle(1, 2, 3, 2);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//    try {
-//      new Rectangle(1, 5, 3, 5);
-//      azzert.fail();
-//    } catch (@SuppressWarnings("unused") final Exception e) {
-//      // an exception has caught
-//    }
-//  }
-
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testGetPerimeter() throws Exception {
-    Assert.assertEquals(new Rectangle(0, 0, -1, -1).getPerimeter(), 4.0, 0.00001);
-    Assert.assertEquals(new Rectangle(0, 0, -2, -2).getPerimeter(), 8.0, 0.00001);
-    Assert.assertEquals(new Rectangle(-1, -1, -2, -2).getPerimeter(), 4.0, 0.00001);
+  @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms3() {
+    forget.it(new Rectangle(1, 2, 1, 4));
+  }
+  @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms4() {
+    forget.it(new Rectangle(5, 2, 5, 4));
+  }
+  @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms5() {
+    forget.it(new Rectangle(1, 2, 3, 2));
+  }
+  @Test(expected = IllegalArgumentException.class )  public void testInvalidCtorParms6() {
+    forget.it(new Rectangle(1, 5, 3, 5));
+  }
+  
+  @Test public void testGetPerimeter() throws IllegalArgumentException{
+    azzert.assertEquals(new Rectangle(0, 0, -1, -1).getPerimeter(), 4.0, 0.00001);
+    azzert.assertEquals(new Rectangle(0, 0, -2, -2).getPerimeter(), 8.0, 0.00001);
+    azzert.assertEquals(new Rectangle(-1, -1, -2, -2).getPerimeter(), 4.0, 0.00001);
   }
 
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testGetArea() throws Exception {
-    Assert.assertEquals(new Rectangle(0, 0, -1, -1).getArea(), 1.0, 0.00001);
-    Assert.assertEquals(new Rectangle(0, 0, -2, -2).getArea(), 4.0, 0.00001);
-    Assert.assertEquals(new Rectangle(-1, -1, -2, -2).getArea(), 1.0, 0.00001);
+  @Test public void testGetArea() throws IllegalArgumentException{
+    azzert.assertEquals(new Rectangle(0, 0, -1, -1).getArea(), 1.0, 0.00001);
+    azzert.assertEquals(new Rectangle(0, 0, -2, -2).getArea(), 4.0, 0.00001);
+    azzert.assertEquals(new Rectangle(-1, -1, -2, -2).getArea(), 1.0, 0.00001);
   }
 
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testGetHight() throws Exception {
-    Assert.assertEquals(new Rectangle(0, 0, -1, -1).getHeight(), 1, 0.00001);
-    Assert.assertEquals(new Rectangle(0, 0, -2, -2).getHeight(), 2, 0.00001);
-    Assert.assertEquals(new Rectangle(-1, -1, -2, -2).getHeight(), 1, 0.00001);
+  @Test public void testGetHight() throws IllegalArgumentException{
+    azzert.assertEquals(new Rectangle(0, 0, -1, -1).getHeight(), 1, 0.00001);
+    azzert.assertEquals(new Rectangle(0, 0, -2, -2).getHeight(), 2, 0.00001);
+    azzert.assertEquals(new Rectangle(-1, -1, -2, -2).getHeight(), 1, 0.00001);
   }
 
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testGetWidth() throws Exception {
-    Assert.assertEquals(new Rectangle(0, 0, -1, -1).getWidth(), 1, 0.00001);
-    Assert.assertEquals(new Rectangle(0, 0, -2, -2).getWidth(), 2, 0.00001);
-    Assert.assertEquals(new Rectangle(-1, -1, -2, -2).getWidth(), 1, 0.00001);
+  @Test public void testGetWidth() throws IllegalArgumentException {
+    azzert.assertEquals(new Rectangle(0, 0, -1, -1).getWidth(), 1, 0.00001);
+    azzert.assertEquals(new Rectangle(0, 0, -2, -2).getWidth(), 2, 0.00001);
+    azzert.assertEquals(new Rectangle(-1, -1, -2, -2).getWidth(), 1, 0.00001);
   }
 
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testExpand() throws Exception {
+  @Test public void testExpand() throws IllegalArgumentException {
     Rectangle r = new Rectangle(0, 0, -1, -1);
     r.expand(2);
-    Assert.assertEquals(r.getArea(), 2, 0.00001);
+    azzert.assertEquals(r.getArea(), 2, 0.00001);
     r = new Rectangle(-1, -1, -2, -2);
     r.expand(2);
-    Assert.assertEquals(r.getArea(), 2, 0.00001);
+    azzert.assertEquals(r.getArea(), 2, 0.00001);
     r = new Rectangle(0, 0, -2, -2);
     r.expand(4);
-    Assert.assertEquals(r.getArea(), 16, 0.00001);
+    azzert.assertEquals(r.getArea(), 16, 0.00001);
   }
 
-  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testRotate() throws Exception {
+  @Test  public void testRotate() throws IllegalArgumentException {
     Rectangle r = new Rectangle(0, 0, 1, 2);
     r.rotate();
-    Assert.assertEquals(r.getHeight(), 1, 0.00001);
-    Assert.assertEquals(r.getWidth(), 2, 0.00001);
+    azzert.assertEquals(r.getHeight(), 1, 0.00001);
+    azzert.assertEquals(r.getWidth(), 2, 0.00001);
     r = new Rectangle(0, 0, 4, 3);
     r.rotate();
-    Assert.assertEquals(r.getHeight(), 4, 0.00001);
-    Assert.assertEquals(r.getWidth(), 3, 0.00001);
+    azzert.assertEquals(r.getHeight(), 4, 0.00001);
+    azzert.assertEquals(r.getWidth(), 3, 0.00001);
   }
 }
