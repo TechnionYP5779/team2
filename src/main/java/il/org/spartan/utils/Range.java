18e6aa741f549b8ebea39929180a9a562c4b03b2
yogi <yogi@192.168.37.208>
2018-11-14 06:53:54 +0200

    Spartanize code

--- src/main/java/il/org/spartan/utils/Range.java
+++ src/main/java/il/org/spartan/utils/Range.java
@@ -2,11 +2,8 @@ package il.org.spartan.utils;
 
 import java.util.*;
 
-/**
- * [[SuppressWarningsSpartan]]
- */
-public class Range{
-  
+/** [[SuppressWarningsSpartan]] */
+public class Range {
   public int from;
   public int to;
 
@@ -14,61 +11,45 @@ public class Range{
     this.from = Integer.MIN_VALUE;
     this.to = Integer.MAX_VALUE - 1;
   }
-  
+
   public Range from(int ¢) {
     this.from = ¢;
     return this;
   }
-  
+
   public Range to(int ¢) {
-    this.to= ¢ - 1;
+    this.to = ¢ - 1;
     return this;
   }
-  
+
   @SuppressWarnings("boxing") public Integer from() {
     return from == Integer.MIN_VALUE ? null : this.from;
   }
-  
-  
+
   public boolean includes(int ¢) {
-    return ¢>=this.from && ¢<= this.to;
+    return ¢ >= this.from && ¢ <= this.to;
   }
-  
-  
-  /**
-   * [[SuppressWarningsSpartan]]
-   */
-  public Iterator<Integer> numbers(){
-    
+
+  /** [[SuppressWarningsSpartan]] */
+  public Iterator<Integer> numbers() {
     int iteratorTo = this.to;
     int iteratorFrom = this.from;
-    
     return new Iterator<Integer>() {
-      
       int current = iteratorFrom;
-      
+
       @Override @SuppressWarnings("boxing") public Integer next() {
         return ++current;
       }
-      
+
       @Override public boolean hasNext() {
         return current < iteratorTo;
       }
     };
   }
 
-  /**
-   * [[SuppressWarningsSpartan]]
-   */
   @SuppressWarnings("hiding") public Range intersect(Range r) {
     int from = Math.max(this.from, r.from);
     int to = Math.min(this.to, r.to) + 1;
-    
-    return from < to ? new Range().from(from).to(to) : null;
+    return from >= to ? null : new Range().from(from).to(to);
   }
-
-
-  
 }
-
-
