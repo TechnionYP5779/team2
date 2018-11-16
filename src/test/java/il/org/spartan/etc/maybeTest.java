abdaf59e1cc98ebafbc1d6a39e59cb75425f7583
unknown <guys4000@gmail.com>
2018-11-16 20:04:12 +0200

    moved tests' files to test hierachy #77

---
+++ src/test/java/il/org/spartan/etc/maybeTest.java
@@ -0,0 +1,21 @@
+package il.org.spartan.etc;
+
+import org.junit.*;
+
+import fluent.ly.*;
+import il.org.spartan.etc.*;
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings("static-method") public class maybeTest {
+  @Test public void usecase0() {
+    azzert.isNull(maybe.no().get());
+  }
+
+  @Test public void usecase1() {
+    azzert.isNull(maybe.yes(null).get());
+  }
+
+  @Test public void usecase2() {
+    assert maybe.yes(new Object()).get() != null;
+  }
+}
