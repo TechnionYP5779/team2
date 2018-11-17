79fa941520bcd938abdb2ea72138505e567dc031
JS_MM <jsmm0202@gmail.com>
2018-11-17 14:49:33 +0200

    split tests in TruthTest #19

--- src/test/java/il/org/spartan/utils/TruthTest.java
+++ src/test/java/il/org/spartan/utils/TruthTest.java
@@ -10,65 +10,114 @@ import fluent.ly.*;
   /**
    *
    */
-  @Test public void truthOfTest() {
+  @Test public void truthOftTest() {
     Truth t = Truth.truthOf(() -> true);
     azzert.that(t, is(Truth.T));
-    t = Truth.truthOf(() -> false);
+  }
+  
+  @Test public void truthOffTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t, is(Truth.F));
-    t = Truth.truthOf(null);
+  }
+  
+  @Test public void truthOfnTest() {
+    Truth t = Truth.truthOf(null);
     azzert.that(t, is(Truth.N));
-    t = Truth.truthOf(() -> {
+  }
+  
+  @Test public void truthOfxTest() {
+    Truth t = Truth.truthOf(() -> {
       assert false;
-      return false;
+      return true;
     });
     azzert.that(t, is(Truth.X));
-    t = Truth.truthOf(() -> 1 / 0 == 1);
+  }
+  
+  @Test public void truthOfrTest() {
+    Truth t = Truth.truthOf(() -> 1 / 0 == 1);
     azzert.that(t, is(Truth.R));
-    t = Truth.truthOf(() -> {
+  }
+  
+  @Test public void truthOfhTest() {
+    Truth t = Truth.truthOf(() -> {
       throw new ExceptionInInitializerError();
     });
     azzert.that(t, is(Truth.Ħ));
   }
-
-  @Test public void notTest() {
+  
+  @Test public void notfTest() {
     Truth t = Truth.truthOf(() -> true);
     azzert.that(t.not(), is(Truth.F));
-    t = Truth.truthOf(() -> false);
+  }
+  
+  @Test public void nottTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t.not(), is(Truth.T));
-    t = Truth.truthOf(() -> {
+  }
+  
+  @Test public void nothTest() {
+    Truth t = Truth.truthOf(() -> {
       throw new ExceptionInInitializerError();
     });
     azzert.that(t.not(), is(Truth.Ħ));
   }
-
-  @Test public void orTest() {
+  
+  @Test public void ortfTest() {
     Truth t = Truth.truthOf(() -> true);
     azzert.that(t.or(Truth.F), is(Truth.T));
+  }
+  
+  @Test public void orttTest() {
+    Truth t = Truth.truthOf(() -> true);
     azzert.that(t.or(Truth.T), is(Truth.T));
-    t = Truth.truthOf(() -> false);
+  }
+  
+  @Test public void orftTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t.or(Truth.T), is(Truth.T));
+  }
+  
+  @Test public void orffTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t.or(Truth.F), is(Truth.F));
   }
 
-  @Test public void andTest() {
+  @Test public void andtfTest() {
     Truth t = Truth.truthOf(() -> true);
     azzert.that(t.and(Truth.F), is(Truth.F));
+  }
+  
+  @Test public void andttTest() {
+    Truth t = Truth.truthOf(() -> true);
     azzert.that(t.and(Truth.T), is(Truth.T));
-    t = Truth.truthOf(() -> false);
+  }
+  
+  @Test public void andftTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t.and(Truth.T), is(Truth.F));
+  }
+  
+  @Test public void andffTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that(t.and(Truth.F), is(Truth.F));
   }
 
   @Test public void letterOfTest() {
     assert "true".equals(Truth.letterOf(() -> true));
   }
-
-  @Test public void toStringTest() {
+  
+  @Test public void toStringtTest() {
     Truth t = Truth.truthOf(() -> true);
     azzert.that("true", is(t + ""));
-    t = Truth.truthOf(() -> false);
+  }
+
+  @Test public void toStringfTest() {
+    Truth t = Truth.truthOf(() -> false);
     azzert.that("false", is(t + ""));
-    t = Truth.truthOf(null);
+  }
+  
+  @Test public void toStringnTest() {
+    Truth t = Truth.truthOf(null);
     azzert.that("Null pointer exception", is(t + ""));
   }
 }
