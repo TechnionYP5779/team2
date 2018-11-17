23997446fab4143e8f75471c83f08f7d7ac7dcd1
JS_MM <jsmm0202@gmail.com>
2018-11-17 19:53:34 +0200

    split tests in anonymousTest #22

--- src/test/java/fluent/ly/anonymousTest.java
+++ src/test/java/fluent/ly/anonymousTest.java
@@ -3,35 +3,43 @@ package fluent.ly;
 import static fluent.ly.anonymous.*;
 import static fluent.ly.azzert.*;
 
-import java.util.function.*;
-
+import java.util.*;
 import org.junit.*;
 
 @SuppressWarnings("static-method") public class anonymousTest {
-  @Test public void booleanTest() {
+  @Test public void booleanTest1() {
     assert ly(() -> true);
+  }
+  
+  @Test public void booleanTest2() {
     assert !ly(() -> false);
   }
 
-  @Test public void doubleTest() {
+  @Test public void doubleTest1() {
     azzert.that(1.5, is(ly(() -> 1.5)));
+  }
+  
+  @Test public void doubleTest2() {
     azzert.that(500.0, is(ly(() -> 500.0)));
   }
 
-  @Test public void intTest() {
+  @Test public void intTest1() {
     azzert.that(1, is(ly(() -> 1)));
+  }
+  
+  @Test public void intTest2() {
     azzert.that(500, is(ly(() -> 500)));
   }
 
-  @Test public void longTest() {
-    azzert.that(1325489434L, is(ly(() -> 1325489434L)));
+  @Test public void longTest1() {
     azzert.that(500548943L, is(ly(() -> 500548943L)));
   }
-
-  @Test public void tTest() {
-    assert ly((BooleanSupplier) () -> true);
-    azzert.that(box.it(1), is(ly((IntSupplier) () -> 1)));
-    azzert.that(1.5, is(ly((DoubleSupplier) () -> 1.5)));
-    azzert.that(1325489434L, is(ly((LongSupplier) () -> 1325489434L)));
+  
+  @Test public void longTest2() {
+    azzert.that(1325489434L, is(ly(() -> 1325489434L)));
+  }
+  
+  @Test @SuppressWarnings("null") public void tTest() {
+    azzert.that(anonymous.ly(() -> Collections.EMPTY_LIST), isA(List.class));
   }
 }
