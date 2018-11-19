89ebd4cee7a28b80beb46594c87c8d1e218d8ae4
unknown <guys4000@gmail.com>
2018-11-19 12:17:14 +0200

    suppressed warnings - OuterTest.java, #91

--- src/test/java/il/org/spartan/utils/OuterTest.java
+++ src/test/java/il/org/spartan/utils/OuterTest.java
@@ -1,85 +1,77 @@
 package il.org.spartan.utils;
 
-import static org.junit.Assert.*;
+import static fluent.ly.azzert.*;
+
+import static fluent.ly.box.*;
 
 import org.junit.*;
 
 import fluent.ly.*;
-import static fluent.ly.azzert.*;
 
-public class OuterTest {
+@SuppressWarnings("static-method") public class OuterTest {
   @Test public void testHashCode1() {
-    Outer<Integer> o = new Outer<>(box.box(1));
+    final Outer<Integer> o = new Outer<>(box(1));
     o.inner = null;
     azzert.assertZero(o.hashCode());
   }
 
   @Test public void testHashCode2() {
-    Outer<Integer> o = new Outer<>(box.box(1));
-    azzert.that(o.hashCode(), is(box.box(1).hashCode()));
+    azzert.that((new Outer<>(box(1))).hashCode(), is(box(1).hashCode()));
   }
-
 
   @Test public void testEqualsObject1() {
-    Object o = new Outer<>(box.box(1));
+    final Object o = new Outer<>(box(1));
     assert o.equals(o);
   }
-  
+
   @Test public void testEqualsObject2() {
-    Object o = new Outer<>(box.box(1));
-    assert !o.equals(null);
+    azzert.notNull(new Outer<>(box(1)));
   }
-  
-  @Test public void testEqualsObject3() {
-    Object o = new Outer<>(box.box(1));
-    assert !o.equals(box.box(1));
+
+  @SuppressWarnings("unlikely-arg-type") @Test public void testEqualsObject3() {
+    assert !(new Outer<>(box(1))).equals(box(1));
   }
-  
+
   @Test public void testEqualsObject4() {
-    Object o1 = new Outer<>(box.box(1));
-    Object o2 = new Outer<>(box.box(2));
-    assert !o1.equals(o2);
+    assert !(new Outer<>(box(1))).equals(new Outer<>(box(2)));
   }
 
   @Test public void testEqualsOuterOfInner1() {
-    Outer<Integer> o1 = new Outer<>(box.box(1));
+    final Outer<Integer> o1 = new Outer<>(box(1));
     o1.set(null);
-    Outer<Integer> o2 = new Outer<>(box.box(1));
-    assert !o1.equals(o2);
+    assert !o1.equals(new Outer<>(box(1)));
   }
-  
+
   @Test public void testEqualsOuterOfInner2() {
-    Outer<Integer> o1 = new Outer<>(box.box(1));
+    final Outer<Integer> o1 = new Outer<>(box(1));
     o1.set(null);
-    Outer<Integer> o2 = new Outer<>(box.box(1));
+    final Outer<Integer> o2 = new Outer<>(box(1));
     o2.set(null);
     assert o1.equals(o2);
   }
-  
+
   @Test public void testEqualsOuterOfInner3() {
-    Outer<Integer> o1 = new Outer<>(box.box(1));
-    Outer<Integer> o2 = new Outer<>(box.box(1));
-    assert o1.equals(o2);
+    assert (new Outer<>(box(1))).equals(new Outer<>(box(1)));
   }
 
   @Test public void testGet() {
-    azzert.that(new Outer<>(box.box(1)).get(), is(box.box(1)));
+    azzert.that(new Outer<>(box(1)).get(), is(box(1)));
   }
 
   @Test public void testSet() {
-    Outer<Integer> o = new Outer<>(box.box(1));
-    o.set(box.box(2));
-    azzert.that(o.get(), is(box.box(2)));
+    final Outer<Integer> o = new Outer<>(box(1));
+    o.set(box(2));
+    azzert.that(o.get(), is(box(2)));
   }
 
   @Test public void testToString1() {
-    Outer<Integer> o = new Outer<>(box.box(1));
+    final Outer<Integer> o = new Outer<>(box(1));
     o.inner = null;
-    azzert.that(o.toString(), is("null"));
+    azzert.that(o + "", is("null"));
   }
-  
+
   @Test public void testToString2() {
-    Outer<Integer> o = new Outer<>(box.box(1));
-    azzert.that(o.toString(), is(o.get().toString()));
+    final Outer<Integer> o = new Outer<>(box(1));
+    azzert.that(o + "", is(o.get() + ""));
   }
 }
