16ea44f785c157955a2b65d7424b9dd629bdd3b3
DorKab <dor.kabareti@campus.technion.ac.il>
2018-11-20 09:14:26 +0200

    fixes travis

--- src/test/java/fluent/ly/outTest.java
+++ src/test/java/fluent/ly/outTest.java
@@ -2,10 +2,36 @@ package fluent.ly;
 
 import static org.junit.Assert.*;
 
+import java.io.*;
+
 import org.junit.*;
 
+import fluent.ly.azzert;
+
+@SuppressWarnings({"static-access", "static-method"})
+
+
+
+/*
+ * In all of the test methods we need to redirect to console output
+ * (that was created with System.out.print) to a String object,
+ * so we can test it
+ */
 public class outTest {
-  @Test public void test() {
-    fail("Not yet implemented");
+   @Test public void testOutString() {
+    String s = "Hello world";
+    ByteArrayOutputStream ba = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(ba));
+    out.out(s);
+    azzert.assertEquals(s, ba + "");
   }
+   
+   @Test public void testOutStringBoolean() {
+     String s = "Hello world";
+     boolean b = true;
+     ByteArrayOutputStream ba = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(ba));
+     out.out(s, b);
+     azzert.assertEquals(s + " = " + b + "\n", ba + "");
+   }
 }
