6c6fbed98cb421a1c098f730e41af382865a8b0a
DorKab <dor.kabareti@campus.technion.ac.il>
2018-11-20 20:52:57 +0200

    changed back assert of boolean (which is ok), finished fixing assertEquals to azzert.that #104

--- src/test/java/il/org/spartan/tideTest.java
+++ src/test/java/il/org/spartan/tideTest.java
@@ -5,23 +5,25 @@ import static fluent.ly.azzert.*;
 import org.jetbrains.annotations.*;
 import org.junit.*;
 
+import fluent.ly.*;
+
 @SuppressWarnings("static-method") public class tideTest {
   @Test public void testClean1() {
-    Assert.assertThat(tide.clean("    code"), is("code"));
+    azzert.that(tide.clean("    code"), is("code"));
   }
 
   @Test public void testClean2() {
-    Assert.assertThat(tide.clean("code      "), is("code"));
+    azzert.that(tide.clean("code      "), is("code"));
   }
 
   @Test public void testClean3() {
     for (final @NotNull String operator : new @NotNull String[] { ":", "/", "%", ",", "=", ":", ";", ">", ">=", "!=", "==", "<", "<=", "-" })
-      Assert.assertThat(tide.clean("code    " + operator + "   code"), is("code" + operator + "code"));
+      azzert.that(tide.clean("code    " + operator + "   code"), is("code" + operator + "code"));
   }
 
   @Test public void testClean4() {
     for (final @NotNull String operator : new @NotNull String[] { "\\{", "\\}", "\\?", "\\+", "\\*", "\\|", "\\&", "\\(", "\\)" })
-      Assert.assertThat(tide.clean("code    " + operator + "   code"), is("code " + operator + "code"));
+      azzert.that(tide.clean("code    " + operator + "   code"), is("code " + operator + "code"));
   }
 
   @Test public void testEq1() {
