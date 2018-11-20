d65821fc36c64cd9ef43bbe75c85ff84d6a8fecd
Yossi Gil <Yossi.Gil@gmail.com>
2018-11-20 05:14:15 +0200

    format

--- src/test/java/il/org/spartan/UtilsTest.java
+++ src/test/java/il/org/spartan/UtilsTest.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.*;
 import org.junit.*;
 
 import fluent.ly.*;
+import il.org.spartan.Utils.*;
 import il.org.spartan.Utils.FoundHandleForT.*;
 import il.org.spartan.etc.*;
 
