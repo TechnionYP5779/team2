db109a4703eb25f6c5c8dccfdbeb9364848cd774
Dor Kabareti <dor.kabareti@campus.technion.ac.il>
2018-12-10 21:56:13 +0200

    Fixed sonar cloud problems #181

--- src/main/java/il/org/parking/Moment.java
+++ src/main/java/il/org/parking/Moment.java
@@ -6,18 +6,18 @@ import java.time.*;
  * a private field.
  * @author DorKabareti */
 public class Moment implements DateTime {
-  private LocalDateTime moment;
+  private LocalDateTime savedMoment;
 
   /** The empty constructor stores the current moment */
   public Moment() {
-    this.moment = LocalDateTime.now();
+    this.savedMoment = LocalDateTime.now();
   }
 
-  /** @param date: a LocalDate object that represents a date (day, month, year)
-   * @param time: a LocalTime object that represents a time (at least hour and a
-   *        minute) */
+  /** @param date a LocalDate object that represents a date (day, month, year)
+   * @param time a LocalTime object that represents a time (at least hour and a
+   *             minute) */
   public Moment(LocalDate date, LocalTime time) {
-    this.moment = LocalDateTime.of(date, time);
+    this.savedMoment = LocalDateTime.of(date, time);
   }
 
   /** This constructor gets 5 parameters that represents a moment in time, creates
@@ -28,12 +28,12 @@ public class Moment implements DateTime {
    * @param hour
    * @param minute */
   public Moment(int year, Month month, int dayOfMonth, int hour, int minute) {
-    this.moment = LocalDateTime.of(LocalDate.of(year, month, dayOfMonth), LocalTime.of(hour, minute));
+    this.savedMoment = LocalDateTime.of(LocalDate.of(year, month, dayOfMonth), LocalTime.of(hour, minute));
   }
 
   /** get the saved LocalDateTime
    * @return the moment saved in the instance of the current Moment object */
   @Override public LocalDateTime getDate() {
-    return moment;
+    return savedMoment;
   }
 }
