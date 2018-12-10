0c964c1c1bc6eed49716d9333e0187b934801b3b
Omer Glick <ogluck35@gmail.com>
2018-12-10 22:41:54 +0200

    added tests for userImplemantation #196

--- src/test/java/il/org/parking/UserImplementationTest.java
+++ src/test/java/il/org/parking/UserImplementationTest.java
@@ -2,34 +2,66 @@ package il.org.parking;
 
 import org.junit.*;
 import org.mockito.*;
+import org.mockito.Mockito.*;
 
 public class UserImplementationTest {
-  
-  private static User user;
-  
-  @BeforeClass
-  public static void setup() {
-    DataBase db = Mockito.mock(DataBase.class);
-    user = new UserImplementation(12345, "tomy", "tomboy@hotmail.com", db);
+  private User user;
+  private DataBase db;
+
+  @Before public void setup() {
+    this.db = Mockito.mock(DataBase.class);
+    this.user = new UserImplementation(12345, "tomy", "tomboy@hotmail.com", db);
   }
-  
-  @Test public static void testGetId() {
+
+  @Test public void testGetId() {
     assert user.getId() == 12345;
   }
-  
-  @Test public static void testGetId2() {
+
+  @Test public void testGetId2() {
     assert user.getId() != 123456;
   }
-  
-  @Test public static void testGetName() {
+
+  @Test public void testGetName() {
     assert user.getName() == "tomy";
   }
-  
-  @Test public static void testGeName2() {
+
+  @Test public void testGeName2() {
     assert user.getName() != "timoty";
   }
-  
-  @Test public static void testContactInfo() {
+
+  @Test public void testContactInfo() {
     assert user.getContactInfo() == "tomboy@hotmail.com";
+  }
+  
+  @Test public void testContactInfo2() {
+    assert user.getContactInfo() != "tomboy@gmail.com";
+  }
+  
+  @Test public void testUpdate() {
+    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    user.update(newUser);
+    
+    assert user.getId() == 111;
+  }
+  
+  @Test public void testUpdate2() {
+    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    user.update(newUser);
+    
+    assert user.getName() == "cohen";
+  }
+  
+  @Test public void testUpdate3() {
+    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    user.update(newUser);
+    
+    assert user.getContactInfo() == "coco@hotmail.com";
+  }
+  
+  @Test public void testUpdate4() {
+    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    user.update(newUser);
+    
+    Mockito.verify(db, Mockito.times(1)).update(12345, newUser);
   }
 }
