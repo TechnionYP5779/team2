8c17fb7456d631fc8fe88a8cc760136af65cfcf9
Omer Glick <ogluck35@gmail.com>
2018-12-10 22:53:40 +0200

    formated tests for userImplementation #196

--- src/test/java/il/org/parking/UserImplementationTest.java
+++ src/test/java/il/org/parking/UserImplementationTest.java
@@ -2,66 +2,62 @@ package il.org.parking;
 
 import org.junit.*;
 import org.mockito.*;
-import org.mockito.Mockito.*;
+
 
 public class UserImplementationTest {
   private User user;
   private DataBase db;
 
-  @Before public void setup() {
+  @Before @SuppressWarnings("null") public void setup() {
     this.db = Mockito.mock(DataBase.class);
-    this.user = new UserImplementation(12345, "tomy", "tomboy@hotmail.com", db);
+    this.user = new UserImplementation(Integer.valueOf(12345), "tomy", "tomboy@hotmail.com", db);
   }
 
   @Test public void testGetId() {
-    assert user.getId() == 12345;
+    assert user.getId().equals(Integer.valueOf(12345));
   }
 
   @Test public void testGetId2() {
-    assert user.getId() != 123456;
+    assert !user.getId().equals(Integer.valueOf(123456));
   }
 
   @Test public void testGetName() {
-    assert user.getName() == "tomy";
+    assert "tomy".equals(user.getName());
   }
 
   @Test public void testGeName2() {
-    assert user.getName() != "timoty";
+    assert !"timoty".equals(user.getName());
   }
 
   @Test public void testContactInfo() {
-    assert user.getContactInfo() == "tomboy@hotmail.com";
+    assert "tomboy@hotmail.com".equals(user.getContactInfo());
   }
-  
+
   @Test public void testContactInfo2() {
-    assert user.getContactInfo() != "tomboy@gmail.com";
+    assert !"tomboy@gmail.com".equals(user.getContactInfo());
   }
-  
+
   @Test public void testUpdate() {
-    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    User newUser = new UserImplementation(Integer.valueOf(111), "cohen", "coco@hotmail.com", db);
     user.update(newUser);
-    
-    assert user.getId() == 111;
+    assert user.getId().equals(Integer.valueOf(111));
   }
-  
+
   @Test public void testUpdate2() {
-    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    User newUser = new UserImplementation(Integer.valueOf(111), "cohen", "coco@hotmail.com", db);
     user.update(newUser);
-    
-    assert user.getName() == "cohen";
+    assert "cohen".equals(user.getName());
   }
-  
+
   @Test public void testUpdate3() {
-    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    User newUser = new UserImplementation(Integer.valueOf(111), "cohen", "coco@hotmail.com", db);
     user.update(newUser);
-    
-    assert user.getContactInfo() == "coco@hotmail.com";
+    assert "coco@hotmail.com".equals(user.getContactInfo());
   }
-  
+
   @Test public void testUpdate4() {
-    User newUser = new UserImplementation(111, "cohen", "coco@hotmail.com", db);
+    User newUser = new UserImplementation(Integer.valueOf(111), "cohen", "coco@hotmail.com", db);
     user.update(newUser);
-    
-    Mockito.verify(db, Mockito.times(1)).update(12345, newUser);
+    Mockito.verify(db, Mockito.times(1)).update(Integer.valueOf(12345), newUser);
   }
 }
