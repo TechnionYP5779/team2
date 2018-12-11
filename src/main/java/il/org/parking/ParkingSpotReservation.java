42129996a48f41c0c4667387235739e1edb37e1b
Jessica Shelly Milgram <jsmm0202@gmail.com>
2018-12-11 19:34:17 +0200

    Implemented Reservation interface #179

---
+++ src/main/java/il/org/parking/ParkingSpotReservation.java
@@ -0,0 +1,46 @@
+package il.org.parking;
+
+/** This class implements the Reservation interface. 
+ * @author JessicaMilgram */
+public class ParkingSpotReservation implements Reservation {
+  
+  private User buyer;
+  private User seller;
+  private double price;
+  private int duration;
+  private DateTime startTime;
+  private int id;
+
+  public ParkingSpotReservation(User buyerIn, User sellerIn, double priceIn, int durationIn, DateTime startTimeIn, int idIn) {
+    buyer = buyerIn;
+    seller = sellerIn;
+    price = priceIn;
+    duration = durationIn;
+    startTime = startTimeIn;
+    id = idIn;
+  }
+  
+  @Override public User getBuyer() {
+    return buyer;
+  }
+
+  @Override public User getSeller() {
+    return seller;
+  }
+
+  @Override public double getPrice() {
+    return price;
+  }
+
+  @Override public int getDuration() {
+    return duration;
+  }
+
+  @Override public DateTime getStartTime() {
+    return startTime;
+  }
+
+  @Override public int getId() {
+    return id;
+  }
+}
