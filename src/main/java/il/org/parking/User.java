0d69efa3d3c3ca136b15c9ff2f901c4d8b4db9fc
Dor Kabareti <dor.kabareti@campus.technion.ac.il>
2018-12-11 17:53:02 +0200

    Updated User interface + documentation #152

--- src/main/java/il/org/parking/User.java
+++ src/main/java/il/org/parking/User.java
@@ -1,27 +1,29 @@
 package il.org.parking;
 
+import java.util.*;
+
+/** This class represents a user in the parking system. a user has an id, name,
+ * contact info (such as phone number, mail, ...), list of parking spots he
+ * rents to others, and list of parking spots he rents from other.
+ * @author Omer Glick */
 public interface User {
-  /**
-   * 
-   * @return sser id
-   */
+  /** @return user id */
   Integer getId();
-  
-  /**
-   * 
-   * @return user name
-   */
+
+  /** @return user name */
   String getName();
-  
-  /**
-   * 
-   * @return user contact info
-   */
+
+  /** @return user contact info */
   String getContactInfo();
-  
-  /**
-   * 
-   * @param updatedUser - new user information to this update user
-   */
+
+  /** @param updatedUser new user information to this update user */
   void update(User updatedUser);
+
+  /** @return a collection of all the parking spots the user rents FROM other
+   *         users right now */
+  Collection<ParkingSpot> getParkingsRentsFromOthers();
+
+  /** @return a collection of all the parking spots the user rents TO other users
+   *         right now */
+  Collection<ParkingSpot> getParkingsRentsToOthers();
 }
