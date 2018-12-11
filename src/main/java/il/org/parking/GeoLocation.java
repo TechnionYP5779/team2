bbf83aa6c95834c277416636acb4c866adbad700
Unknown <talporat@campus.technion.ac.il>
2018-12-11 16:29:47 +0200

    add some changes to GeoLocation - issue #177

--- src/main/java/il/org/parking/GeoLocation.java
+++ src/main/java/il/org/parking/GeoLocation.java
@@ -9,11 +9,13 @@ import fluent.ly.*;
 
 public class GeoLocation implements Location{
 
-  private Double latitude;
-  private Double longitude;
+  private double latitude;
+  private double longitude;
   private String address;
   
-  public GeoLocation(Double latitude ,Double longitude){
+  public GeoLocation(double latitude ,double longitude){
+    if(latitude > 90 || latitude < -90 || longitude > 180 || longitude < -180)
+      throw new IllegalArgumentException();
     this.latitude = latitude;
     this.longitude = longitude;
   }
@@ -23,24 +25,24 @@ public class GeoLocation implements Location{
     String formattedAddress = URLEncoder.encode(address,"UTF-8"); 
     String URL = "https://nominatim.openstreetmap.org/search?q="+formattedAddress+"&format=json&addressdetails=1";
     String json = GeoLocation.getRequest(URL);
-    this.latitude = Double.valueOf(JsonPath.read(json,"$[0].lat"));
-    this.longitude = Double.valueOf(JsonPath.read(json,"$[0].lon"));
+    this.latitude = Double.valueOf(JsonPath.read(json,"$[0].lat")).doubleValue();
+    this.longitude =  Double.valueOf(JsonPath.read(json,"$[0].lon")).doubleValue();
   }
   
   // Calculate the air distance between two geographic coordinates with the Haversine formula
   @Override public Double calcDist(Location other) {
-    if( !(other instanceof GeoLocation) )
+    if( !(other instanceof GeoLocation))
       return null ;
     final int $ = 6371; // Radius of the earth
-    double lat1 = this.latitude.doubleValue();
-    double lon1 = this.longitude.doubleValue();
-    double lat2 = ((GeoLocation)other).latitude.doubleValue();
-    double lon2 = ((GeoLocation)other).longitude.doubleValue();
+    double lat1 = this.latitude;
+    double lon1 = this.longitude;
+    double lat2 = ((GeoLocation)other).latitude;
+    double lon2 = ((GeoLocation)other).longitude;
     double latDistance = Math.toRadians(lat2-lat1);
     double lonDistance = Math.toRadians(lon2-lon1);
     double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + 
-    Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * 
-    Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * 
+        Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
     double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     return box.it($ * c);
   }
--- src/test/java/il/org/parking/GeoLocationTest.java
+++ src/test/java/il/org/parking/GeoLocationTest.java
@@ -5,7 +5,24 @@ import org.junit.*;
 import fluent.ly.*;
 
 @SuppressWarnings({"static-method","static-access"}) 
+
 public class GeoLocationTest {
+  
+  @Test(expected=IllegalArgumentException.class) public void ctor1() {
+    forget.unused(new GeoLocation(91.0,0.0));
+  }
+  @Test(expected=IllegalArgumentException.class) public void ctor2() {
+    forget.unused(new GeoLocation(-91.0,0.0));
+  }
+  @Test(expected=IllegalArgumentException.class) public void ctor3() {
+    forget.unused(new GeoLocation(0.0,181.0));
+  }
+  @Test(expected=IllegalArgumentException.class) public void ctor4() {
+    forget.unused(new GeoLocation(0.0,-181.0));
+  }
+  @Test public void ctor5() {
+    forget.unused(new GeoLocation(0.0,0.0));
+  }
   @Test public void calcDistTest1() throws Exception {
     azzert.assertEquals(new GeoLocation("תל אביב").calcDist(new GeoLocation("חיפה")).doubleValue(),84.63,0.01);
   }
@@ -20,5 +37,8 @@ public class GeoLocationTest {
   }
   @Test public void calcDistTest5() throws Exception {
     azzert.assertEquals(new GeoLocation("הטכניון חיפה").calcDist(new GeoLocation("חיפה חוף הכרמל")).doubleValue(),6.35,0.01);
+  }
+  @Test public void calcDistTest6() throws Exception {
+    azzert.assertNull(new GeoLocation("תל אביב").calcDist(null));
   }
 }
