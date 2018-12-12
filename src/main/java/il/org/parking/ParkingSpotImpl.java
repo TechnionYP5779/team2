111aeb4c3e48b744984b4fbeabe627d0abce39f3
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-12 09:58:45 +0200

    fixed and updated implementation of ParkingSpot

--- src/main/java/il/org/parking/ParkingSpotImpl.java
+++ src/main/java/il/org/parking/ParkingSpotImpl.java
@@ -1,35 +1,51 @@
 package il.org.parking;
 
 public class ParkingSpotImpl implements ParkingSpot {
-  private Location location;
-  private Price price;
+  private Integer id;
   private Availability avail;
+  private User seller;
+  private double price;
+  private Location location;
   
-  ParkingSpotImpl(Location l, Price p, Availability a){
-    //need to call create from factory here
-    location = l;
-    price = p;
-    avail = a;
+
+  public ParkingSpotImpl(Integer id, Availability availability, User seller, double price,Location location) {
+    this.id = id;
+    this.avail = availability;
+    this.seller = seller;
+    this.price = price;
+    this.location = location;
+  }
+
+  @Override public Integer getId() {
+    return this.id;
+  }
+  
+  @Override public User getSeller() {
+    return this.seller;
+  }
+  
+  @Override public double getPrice() {
+    return this.price;
   }
   
   @Override public Location getLocation() {
-    return location;
+    return this.location;
   }
-
-  @Override public Price getPrice() {
-    return price;
-  }
-
+  
   @Override public Availability getAvailability() {
-    return avail;
+    return this.avail;
   }
-  
+
   @Override public void updateLocation(Location newLocation) {
-    location = newLocation;
+    this.location = newLocation;
   }
   
-  @Override public void updatePrice(Price newPrice) {
-    price.update(newPrice);
+  @Override public void updatePrice(double newPrice) {
+    this.price = newPrice;
+  }
+
+  @Override public void decreaseAvailability(Availability other) {
+    this.avail = this.avail.subtraction(other);
   }
   
   @Override public void addAvailability(Availability other) {
@@ -37,7 +53,9 @@ public class ParkingSpotImpl implements ParkingSpot {
     return; // need to add a function for adding to this interval
   }
   
-  @Override public void decreaseAvailability(Availability other) {
-    avail.subtraction(other);
+  @Override public String toString() {
+    return "ParkingSpotImplementation [id=" + id + ", availability=" + avail + ", seller=" + seller + ", price=" + price + "]";
   }
+  
+
 }
